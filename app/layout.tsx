1c43a19cadb687233338b528fc228c633e106d16
SantiagoRamirezArco <santiago.ramirez@arco365.com>
2025-10-27 16:21:37 -0500

    Update: ajustes en layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: 'Celebración 30 años de la figura del Curador Urbano',
-  description: 'Celebración de las tres décadas del Colegio Nacional de Curadores Urbanos. 28-29 de Noviembre, 2025 en Montería y San Antero, Córdoba.',
+  description: 'Celebración 30 años de la figura del Curador Urbano.',
   keywords: ['Curadores Urbanos', 'CNCU', '30 años', 'Montería', 'San Antero', 'Córdoba', 'Evento'],
   authors: [{ name: 'Colegio Nacional de Curadores Urbanos' }],
   icons: {
